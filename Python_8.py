b597cb88e17922395e746e01268deab6ddf6c212
student <student@PC-G-04>
2023-04-23 11:50:28 +0300

    task

--- Python_8.py
+++ Python_8.py
@@ -1,1 +1,9 @@
 
+
+filename = "hello.txt"
+search_word = input("Введіть слово для пошуку: ")
+
+with open(filename, "r") as file:
+    for line in read:
+        if search_word in line:
+            print(line)
