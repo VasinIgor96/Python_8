d0592192aabfc8f81ab8a9cd10b377927994777a
student <student@PC-G-04>
2023-04-23 11:33:06 +0300

    Ex_3 Task done

--- Python_8.py
+++ Python_8.py
@@ -1,1 +1,8 @@
 
+filename = "hello.txt"
+
+with open (filename, "r") as file:
+    content = file.read()
+    words = content.split()
+    num_words = len(words)
+    print("Кількість слів в файлі:", num_words)
