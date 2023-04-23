83ffd72e657d83f5cb3586fe8922e3732ef7bfa9
student <student@PC-G-04>
2023-04-23 11:23:09 +0300

    Ex_2 Task done

--- Python_8.py
+++ Python_8.py
@@ -1,1 +1,12 @@
+filename = "hello.txt"  
 
+search_string = input("Введіть рядок для пошуку: ")
+
+
+with open(filename, "r") as file:
+
+    content = file.read()
+    if search_string in content:
+        print("Рядок знайдено: ", search_string)
+    else:
+        print("Рядок не знайдено: ", search_string)
