888667e6e8562d02eea61e519f6a4aad6b08f17b
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-27 23:56:04 +0300

    task

--- Python_8.py
+++ Python_8.py
@@ -1,9 +1,12 @@
+def replace_line(filename, old_line, new_line):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
 
+    with open(filename, 'w') as file:
+        for line in lines:
+            if line.strip() == old_line.strip():
+                file.write(new_line + '\n')
+            else:
+                file.write(line)
 
-filename = "hello.txt"
-search_word = input("Введіть слово для пошуку: ")
-
-with open(filename, "r") as file:
-    for line in file:
-        if search_word in line:
-            print(line)
+replace_line('hello.txt', 'old text', 'new text')
