388f440175939672dc9ba27f0d9a836b139341be
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-27 22:54:12 +0300

    Ex_4 Task done

--- Python_8.py
+++ Python_8.py
@@ -4,6 +4,6 @@ filename = "hello.txt"
 search_word = input("Введіть слово для пошуку: ")
 
 with open(filename, "r") as file:
-    for line in read:
+    for line in file:
         if search_word in line:
             print(line)
