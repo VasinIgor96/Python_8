bdb49aff65d9ce44ac549bd0282ed047ae68954c
Vasin Igor <126420687+VasinIgor96@users.noreply.github.com>
2023-04-27 21:59:07 +0200

    Update Python_8.py

--- Python_8.py
+++ Python_8.py
@@ -1,9 +1,1 @@
 
-
-filename = "hello.txt"
-search_word = input("Введіть слово для пошуку: ")
-
-with open(filename, "r") as file:
-    for line in file:
-        if search_word in line:
-            print(line)
